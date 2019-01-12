00997e0d23370f82a2ef60fb7739b4ec19f57a61
ayrusme <ayrusme@gmail.com>
2019-01-12 10:41:22 +0530

    Add validation ✅

--- app/database/validator.py
+++ app/database/validator.py
@@ -1,11 +1,36 @@
 """
 The validation schema for the models 
 """
+from schema import And, Forbidden, Optional, Regex, Schema
 
-# TODO Add Validation Schema
-
-USER_SCHEMA = {
-}
+USER_SCHEMA = Schema({
+    Forbidden("guid"): And(str, len),
+    Forbidden("created_at"): And(str, len),
+    Forbidden("updated_at"): And(str, len),
+    "first_name": And(str, len),
+    "last_name": And(str, len),
+    "email": Regex(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"),
+    "phone": Regex(r"[6789]{1}[0-9]{9}"),
+    "addresses": list,
+    "authentication": object
+})
 
 ADDRESS_SCHEMA = {
+    Forbidden("guid"): And(str, len),
+    Forbidden("created_at"): And(str, len),
+    Forbidden("updated_at"): And(str, len),
+    "address_line1": And(str, len),
+    "address_line2": And(str, len),
+    "city": And(str, len),
+    "country": And(str, len),
+    "pin_code": And(int, len),
+    "lat_long": And(int, len)
+}
+
+AUTHENTICATION_SCHEMA = {
+    Forbidden("guid"): And(str, len),
+    Forbidden("created_at"): And(str, len),
+    Forbidden("updated_at"): And(str, len),
+    "username": And(str, len),
+    "password": And(str, len)
 }
