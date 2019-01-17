5d3bbc4063b8a6afc832e74149f62de41a191be3
ayrusme <ayrusme@gmail.com>
2019-01-17 08:42:26 +0530

    Add alembic 🕹

---
+++ app/alembic/versions/84e72bb1b2af_signup_module.py
@@ -0,0 +1,24 @@
+"""signup module
+
+Revision ID: 84e72bb1b2af
+Revises: 
+Create Date: 2019-01-17 08:41:29.818217
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '84e72bb1b2af'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
